5055f606ffe343d322150c610fc96c6414d393ab
Serinajefthas <serina.jefthas@gmail.com>
2024-01-07 22:25:11 +0200

    condense task 12 function

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,12 +1,8 @@
 export default function createReportObject(employeesList) {
-  const allEmployess = {
-    ...employeesList,
-  };
-  const getNumDepartments = (employees) => { // eslint-disable-line no-unused-vars
-    return Object.keys(employees).length;
-  };
-  return {
-    allEmployees, // eslint-disable-line no-unused-vars
-    getNumDepartments,
-  };
+    return {
+        allEmployees: { ...employeesList },
+        getNumberOfDepartments(list) {
+          return Object.keys(list).length;
+        },
+      };
 }
