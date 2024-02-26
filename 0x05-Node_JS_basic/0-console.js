8ac62c01ad1f56e85d46726af9e7d55c7be5287b
Serinajefthas <serina.jefthas@gmail.om>
2024-02-26 21:23:53 +0000

    fix ESLint syntax

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,3 +1,10 @@
+/**
+ * Prints message to STDOUT
+ *
+ * @param {message} str - msg to print
+ */
 function displayMessage(message) {
-	console.log(message);
+  console.log(message);
 }
+
+module.exports = displayMessage;
