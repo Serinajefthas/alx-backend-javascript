6ccd9d7018daca24909a9275f90e65e4d0989394
Serinajefthas <serina.jefthas@gmail.om>
2024-02-27 20:35:05 +0000

    task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,8 +4,8 @@
  */
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('input', (input) => {
-  process.stdout.write(`Your name is: ${input}`);
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
 });
 
 process.stdin.on('end', () => {
